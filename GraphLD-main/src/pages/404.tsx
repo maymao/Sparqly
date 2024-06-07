4a89b1fbb89c48b3cd709313808830ff32b921aa
Qinxin Mao <may3131@Qinxins-Laptop.local>
2024-06-07 23:17:51 +0100

    remove: original version

--- GraphLD-main/src/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-function Page404() {
-  return (
-    <div>
-      <h1>404: not found</h1>
-    </div>
-  );
-}
-
-export default Page404;
--- GraphLD-main/src/store/index.ts
+++
@@ -1,8 +0,0 @@
-import databaseReducer from '@/pages/reducer/databaseReducer';
-import { configureStore } from '@reduxjs/toolkit';
-
-export default configureStore({
-  reducer: {
-    database: databaseReducer,
-  },
-});
